95fd92f40828c3b2e58bafcac4a5b4db3514bc1d
Ksenaaa <103521446+Ksenaaa@users.noreply.github.com>
2024-07-26 20:09:02 +0200

    connected to db, graphql, server (#7)
    Co-authored-by: Ksena <ksena@ip-192-168-1-7.ec2.internal>

---
+++ server/src/resolvers/commentResolvers.ts
@@ -0,0 +1,33 @@
+import Comment from "../models/commentSchema.ts";
+import User from "../models/userSchema.ts";
+
+export const commentTypeDefs = `
+    type Query {
+        comments: [Comment]
+    }
+    type Comment {
+        id: ID!
+        email: String
+        movie_id: ID
+        text: String
+        date: String,
+        user: User
+    }
+`;
+
+export const commentsResolvers = {
+    Query: {
+        comments: async (obj, args) => {
+            const result = await Comment.find().limit(30).exec();
+            return result;
+        },
+    },
+    Comment: {
+        id: (obj) => obj._id || obj.id,
+        user: async ({ email }, args) => {
+            const result = await User.findOne({ email });
+            console.log(result);
+            return result;
+        },
+    },
+};
