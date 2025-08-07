0bde668a3713033ac7905265bb9b1c964586b8b0
Ksenaaa <oksenaka@gmail.com>
2025-08-07 22:48:52 +0200

    added generate-schema on the server

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'http://localhost:5000/graphql',
+    schema: '../server/schema.graphql',
     documents: ['src/**/*.ts'],
     generates: {
         './src/__generated__/': {
---
+++ server/src/generate-schema.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { buildASTSchema, print } from "graphql";
+import { gql } from "graphql-tag";
+
+import { userTypeDefs } from "./resolvers/userResolvers";
+import { commentTypeDefs } from "./resolvers/commentResolvers";
+import { movieTypeDefs } from "./resolvers/movieResolvers";
+import { dateScalarTypeDefs } from "./resolvers/dateScalarResolvers";
+
+const allTypeDefs = gql`
+  ${userTypeDefs}
+  ${commentTypeDefs}
+  ${movieTypeDefs}
+  ${dateScalarTypeDefs}
+`;
+
+const schema = buildASTSchema(allTypeDefs);
+const schemaSDL = print(schema);
+
+fs.writeFileSync(path.resolve(__dirname, "../schema.graphql"), schemaSDL);
